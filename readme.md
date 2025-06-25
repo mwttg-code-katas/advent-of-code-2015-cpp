5af6bde793c731b631b10da60e656606391b49e0
Marco Wittig <marco-wittig@gmx.de>
2025-06-25 20:33:28 +0200

    Solve puzzle 01 part 2

--- readme.md
+++ readme.md
@@ -10,7 +10,7 @@
 
 |       Day       | Puzzle 1 | Puzzle 2 |
 |:---------------:|:--------:|:--------:|
-| [Day 01][day01] |  < 1 ms  |          | 
+| [Day 01][day01] |  < 1 ms  |  < 1 ms  | 
 
 [comment]: # "List of URLs down below, sorted alphabetically DESC by tag"
 [advent-of-code]: https://adventofcode.com/2015/
--- src/day01/main.cpp
+++ src/day01/main.cpp
@@ -5,6 +5,8 @@
 
 int puzzle01();
 
+int puzzle02();
+
 int main() {
     std::chrono::time_point<std::chrono::system_clock> start = std::chrono::high_resolution_clock::now();
     int solution_puzzle01 = puzzle01();
@@ -12,14 +14,34 @@ int main() {
     std::chrono::duration<double, std::milli> duration = end - start;
     std::cout << "The solution of puzzle 01 is: " << solution_puzzle01 << " this took " << duration << " to execute."
             << std::endl;
+
+    std::chrono::time_point<std::chrono::system_clock> start2 = std::chrono::high_resolution_clock::now();
+    int solution_puzzle02 = puzzle02();
+    std::chrono::time_point<std::chrono::system_clock> end2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration2 = end2 - start2;
+    std::cout << "The solution of puzzle 02 is: " << solution_puzzle02 << " this took " << duration2 << " to execute."
+            << std::endl;
 }
 
 int puzzle01() {
-    const std::string input = filereader::read_file("input.txt");
+    const std::string input = filereader::read_file("day01-input.txt");
     int floor = 0;
     for (const char c: input) {
         if (c == '(') floor++;
         if (c == ')') floor--;
     }
     return floor;
+}
+
+int puzzle02() {
+    const std::string input = filereader::read_file("day01-input.txt");
+    int floor = 0;
+    int index = 0;
+    while (floor != -1) {
+        const char c = input[index];
+        if (c == '(') floor++;
+        if (c == ')') floor--;
+        index++;
+    }
+    return index;
 }
