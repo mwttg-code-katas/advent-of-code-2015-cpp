d6bc00e773f275d2a14fc8c374d372564f3d3f8e
Marco Wittig <marco-wittig@gmx.de>
2025-06-25 22:34:24 +0200

    Solve puzzle 02 part 1

--- readme.md
+++ readme.md
@@ -10,8 +10,10 @@
 
 |       Day       | Puzzle 1 | Puzzle 2 |
 |:---------------:|:--------:|:--------:|
-| [Day 01][day01] |  < 1 ms  |  < 1 ms  | 
+| [Day 01][day01] |  < 1 ms  |  < 1 ms  |
+| [Day 02][day02] |   4 ms   |          |
 
 [comment]: # "List of URLs down below, sorted alphabetically DESC by tag"
 [advent-of-code]: https://adventofcode.com/2015/
 [day01]: https://adventofcode.com/2015/day/1
+[day02]: https://adventofcode.com/2015/day/2
---
+++ src/day02/main.cpp
@@ -0,0 +1,51 @@
+#include "filereader.h"
+
+#include <iostream>
+#include <sstream>
+#include <chrono>
+
+std::vector<std::vector<int> > getParsedPuzzleInput(const std::string &filename);
+
+int puzzle01();
+
+int puzzle02();
+
+int main() {
+    std::chrono::time_point<std::chrono::system_clock> start = std::chrono::high_resolution_clock::now();
+    int solution_puzzle01 = puzzle01();
+    std::chrono::time_point<std::chrono::system_clock> end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+    std::cout << "The solution of puzzle 01 is: " << solution_puzzle01 << " this took " << duration << " to execute."
+            << std::endl;
+}
+
+int puzzle01() {
+    const std::vector<std::vector<int> > input = getParsedPuzzleInput("day02-input.txt");
+    int result = 0;
+    for (const std::vector<int>& dimension: input) {
+        const int side1 = dimension[0] * dimension[1];
+        const int side2 = dimension[1] * dimension[2];
+        const int side3 = dimension[2] * dimension[0];
+        const int smallestSide = std::min(side1, std::min(side2, side3));
+        const int size = (2 * side1) + (2 * side2) + (2 * side3) + smallestSide;
+        result = result + size;
+    }
+    return result;
+}
+
+
+std::vector<std::vector<int> > getParsedPuzzleInput(const std::string &filename) {
+    const std::vector<std::string> lines = filereader::read_lines(filename);
+    std::vector<std::vector<int> > result;
+    for (const std::string &line: lines) {
+        std::istringstream ss(line);
+        std::string part;
+        std::vector<int> intParts;
+        while (std::getline(ss, part, 'x')) {
+            int number = stoi(part);
+            intParts.push_back(number);
+        }
+        result.push_back(std::move(intParts));
+    }
+    return result;
+}
