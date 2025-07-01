966667f711ecae719d71274ddd392be3917d230c
Marco Wittig <marco-wittig@gmx.de>
2025-07-01 22:09:49 +0200

    Solve puzzle 04 part 2

--- src/day04/main.cpp
+++ src/day04/main.cpp
@@ -5,6 +5,7 @@
 const std::string secretKey = "yzbqklnj";
 
 unsigned long puzzle01();
+unsigned long puzzle02();
 
 unsigned long mining(int startingZeros);
 
@@ -15,10 +16,21 @@ int main() {
     std::chrono::duration<double, std::milli> duration = end - start;
     std::cout << "The solution of puzzle 01 is: " << solution_puzzle01 << " this took " << duration << " to execute."
             << std::endl;
+
+    std::chrono::time_point<std::chrono::system_clock> start2 = std::chrono::high_resolution_clock::now();
+    const unsigned long solution_puzzle02 = puzzle02();
+    std::chrono::time_point<std::chrono::system_clock> end2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration2 = end2 - start2;
+    std::cout << "The solution of puzzle 02 is: " << solution_puzzle02 << " this took " << duration2 << " to execute."
+            << std::endl;
 }
 
 unsigned long puzzle01() {
     return mining(5);
+}
+
+unsigned long puzzle02() {
+    return mining(6);
 }
 
 unsigned long mining(int startingZeros) {
