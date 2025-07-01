716054683913cdf501c766292d7455a95b04a94f
Marco Wittig <marco-wittig@gmx.de>
2025-07-01 22:07:44 +0200

    Solve puzzle 04 part 1

--- readme.md
+++ readme.md
@@ -13,9 +13,11 @@
 | [Day 01][day01] |  < 1 ms  |  < 1 ms  |
 | [Day 02][day02] |   4 ms   |   4 ms   |
 | [Day 03][day03] |   8 ms   |   5 ms   |
+| [Day 04][day04] |   1 s    |          |
 
 [comment]: # "List of URLs down below, sorted alphabetically DESC by tag"
 [advent-of-code]: https://adventofcode.com/2015/
 [day01]: https://adventofcode.com/2015/day/1
 [day02]: https://adventofcode.com/2015/day/2
 [day03]: https://adventofcode.com/2015/day/3
+[day03]: https://adventofcode.com/2015/day/4
---
+++ src/day04/main.cpp
@@ -0,0 +1,38 @@
+#include "md5.h"
+
+#include <chrono>
+
+const std::string secretKey = "yzbqklnj";
+
+unsigned long puzzle01();
+
+unsigned long mining(int startingZeros);
+
+int main() {
+    std::chrono::time_point<std::chrono::system_clock> start = std::chrono::high_resolution_clock::now();
+    const unsigned long solution_puzzle01 = puzzle01();
+    std::chrono::time_point<std::chrono::system_clock> end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+    std::cout << "The solution of puzzle 01 is: " << solution_puzzle01 << " this took " << duration << " to execute."
+            << std::endl;
+}
+
+unsigned long puzzle01() {
+    return mining(5);
+}
+
+unsigned long mining(int startingZeros) {
+    unsigned long miningNumber = 0;
+    const std::string zeros(startingZeros, '0');
+
+    while (true) {
+        std::string input = secretKey + std::to_string(miningNumber);
+        std::string hex = MD5(input).hexdigest();
+
+        if (hex.substr(0, startingZeros) == zeros) {
+            return miningNumber;
+        }
+
+        miningNumber++;
+    }
+}
